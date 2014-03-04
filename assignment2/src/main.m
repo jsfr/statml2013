93dbd0da48a4b8d279d3cb6e898c6b629e9ab8db
jsfr <jfrdev@gmail.com>
2014-03-04 11:35:09 +0100

    Removed the fugly for-loops, increased interval and updated gitignore to ignor figures and generated pdf

--- assignment2/src/main.m
+++ assignment2/src/main.m
@@ -60,9 +60,10 @@ w3ML = linearRegression(trainData)
 
 y = @(x, wML) [1 x] * wML;
 
-y1 = arrayfun(@(x1, x2) y([x1 x2], w1ML), testData(:,3), testData(:, 4));
-y2 = arrayfun(@(x) y(x, w2ML), testData(:,5));
-y3 = arrayfun(@(x1, x2, x3, x4, x5) y([x1 x2 x3 x4 x5], w3ML), testData(:,1), testData(:,2), testData(:,3), testData(:,4), testData(:,5));
+t1 = arrayfun(@(x1, x2) y([x1 x2], w1ML), testData(:,3), testData(:, 4));
+t2 = arrayfun(@(x) y(x, w2ML), testData(:,5));
+t3 = arrayfun(@(x1, x2, x3, x4, x5) y([x1 x2 x3 x4 x5], w3ML), testData(:,1), ...
+              testData(:,2), testData(:,3), testData(:,4), testData(:,5));
 
 t2Line = arrayfun(@(x) y(x, w2ML), [min(testData(:,5)):0.01:max(testData(:,5))]);
 
@@ -71,7 +72,7 @@ h = figure(1);
 plot(trainData(:,5), trainData(:,6), 'ok', 'MarkerSize', 6, 'MarkerFaceColor','r');
 hold on;
 plot(testData(:,5), testData(:,6), 'ok', 'MarkerSize', 6, 'MarkerFaceColor','g');
-plot(testData(:,5), y2, 'ok', 'MarkerSize', 6, 'MarkerFaceColor','b');
+plot(testData(:,5), t2, 'ok', 'MarkerSize', 6, 'MarkerFaceColor','b');
 plot([min(testData(:,5)):0.01:max(testData(:,5))], t2Line, 'b-', 'LineWidth', 1.5);
 legend('Training data', 'Test data', 'Linear regression');
 ylabel('# of sunspots');
@@ -80,15 +81,15 @@ betterPlots(h);
 print(h, '-depsc2', '../figures/II21_1.eps');
 hold off;
 
-rms1 = sqrt(1/size(testData,1)*sum(testData(:,6)-y1)^2)
-rms2 = sqrt(1/size(testData,1)*sum(testData(:,6)-y2)^2)
-rms3 = sqrt(1/size(testData,1)*sum(testData(:,6)-y3)^2)
+rms1 = sqrt(1/size(testData,1)*sum(testData(:,6)-t1)^2)
+rms2 = sqrt(1/size(testData,1)*sum(testData(:,6)-t2)^2)
+rms3 = sqrt(1/size(testData,1)*sum(testData(:,6)-t3)^2)
 
 plot([1916:1:2011], testData(:, 6), 'k-', 'LineWidth', 1.5);
 hold on;
-plot([1916:1:2011], y1, 'r-', 'LineWidth', 1.5);
-plot([1916:1:2011], y2, 'g-', 'LineWidth', 1.5);
-plot([1916:1:2011], y3, 'b-', 'LineWidth', 1.5);
+plot([1916:1:2011], t1, 'r-', 'LineWidth', 1.5);
+plot([1916:1:2011], t2, 'g-', 'LineWidth', 1.5);
+plot([1916:1:2011], t3, 'b-', 'LineWidth', 1.5);
 legend('Test data', 'Model 1', 'Model 2', 'Model 3');
 ylabel('# of sunspots');
 xlabel('Year');
@@ -109,35 +110,34 @@ rms3ML = rms3;
 trainData = importdata('../data/sunspotsTrainStatML.dt');
 testData = importdata('../data/sunspotsTestStatML.dt');
 
-w1MAP = arrayfun(@(x) maxPosterior([trainData(:,3:4) trainData(:, 6)], x, 1), [0:0.001:1], 'UniformOutput', false);
-w2MAP = arrayfun(@(x) maxPosterior(trainData(:,5:6), x, 1), [0:0.001:1], 'UniformOutput', false);
-w3MAP = arrayfun(@(x) maxPosterior(trainData, x, 1), [0:0.001:1], 'UniformOutput', false);
+w1MAP = arrayfun(@(x) maxPosterior([trainData(:,3:4) trainData(:, 6)], x, 1), ...
+                 [0:0.1:200], 'UniformOutput', false);
+w2MAP = arrayfun(@(x) maxPosterior(trainData(:,5:6), x, 1), ...
+                 [-50:0.1:10], 'UniformOutput', false);
+w3MAP = arrayfun(@(x) maxPosterior(trainData, x, 1), ...
+                 [0:0.1:200], 'UniformOutput', false);
 
 y = @(x, wMAP) [1 x] * wMAP;
+rms = @(ts) cell2mat(cellfun(@(t) sqrt(1/size(testData,1)*sum(testData(:,6)-t)^2), ...
+                     ts, 'UniformOutput', false));
 
-rms1 = [];
-for k=1:length(w1MAP)
-    y1 = arrayfun(@(x1, x2) y([x1 x2], w1MAP{k}), testData(:,3), testData(:, 4));
-    rms1 = [rms1 sqrt(1/size(testData,1)*sum(testData(:,6)-y1)^2)];
-end %TODO make pretty when time!
+t1 = cellfun(@(w) arrayfun(@(x1, x2) y([x1 x2], w), testData(:,3), ...
+             testData(:, 4)), w1MAP, 'UniformOutput', false);
+rms1 = rms(t1);
 
-rms2 = [];
-for k=1:length(w2MAP)
-    y2 = arrayfun(@(x) y(x, w2MAP{k}), testData(:,5));
-    rms2 = [rms2 sqrt(1/size(testData,1)*sum(testData(:,6)-y2)^2)];
-end %TODO make pretty when time!
+t2 = cellfun(@(w) arrayfun(@(x) y(x, w), testData(:,5)), w2MAP, 'UniformOutput', false);
+rms2 = rms(t2);
 
-rms3 = [];
-for k=1:length(w3MAP)
-    y3 = arrayfun(@(x1, x2, x3, x4, x5) y([x1 x2 x3 x4 x5], w3MAP{k}), testData(:,1), testData(:,2), testData(:,3), testData(:,4), testData(:,5));
-    rms3 = [rms3 sqrt(1/size(testData,1)*sum(testData(:,6)-y3)^2)];
-end %TODO make pretty when time!
+t3 = cellfun(@(w) arrayfun(@(x1, x2, x3, x4, x5) y([x1 x2 x3 x4 x5], w), ...
+             testData(:,1), testData(:,2), testData(:,3), testData(:,4), ...
+             testData(:,5)), w3MAP, 'UniformOutput', false);
+rms3 = rms(t3);
 
 figure(h)
 
-plot([0:0.001:1], rms1, 'r-', 'LineWidth', 1.5);
+plot([0:0.1:200], rms1, 'r-', 'LineWidth', 1.5);
 hold on;
-plot([0:0.001:1], repmat(rms1ML, 1, size(rms1, 2)), 'b--', 'LineWidth', 1.5);
+plot([0:0.1:200], repmat(rms1ML, 1, size(rms1, 2)), 'b--', 'LineWidth', 1.5);
 ylabel('RMS');
 xlabel('Alpha');
 legend('RMS_{MAP}', 'RMS_{ML}');
@@ -145,9 +145,9 @@ betterPlots(h);
 print(h, '-depsc2', '../figures/II22_1.eps');
 hold off;
 
-plot([0:0.001:1], rms2, 'r-', 'LineWidth', 1.5);
+plot([-50:0.1:10], rms2, 'r-', 'LineWidth', 1.5);
 hold on;
-plot([0:0.001:1], repmat(rms2ML, 1, size(rms2, 2)), 'b--', 'LineWidth', 1.5);
+plot([-50:0.1:10], repmat(rms2ML, 1, size(rms2, 2)), 'b--', 'LineWidth', 1.5);
 ylabel('RMS');
 xlabel('Alpha');
 legend('RMS_{MAP}', 'RMS_{ML}');
@@ -155,9 +155,9 @@ betterPlots(h);
 print(h, '-depsc2', '../figures/II22_2.eps');
 hold off;
 
-plot([0:0.001:1], rms3, 'r-', 'LineWidth', 1.5);
+plot([0:0.1:200], rms3, 'r-', 'LineWidth', 1.5);
 hold on;
-plot([0:0.001:1], repmat(rms3ML, 1, size(rms3, 2)), 'b--', 'LineWidth', 1.5);
+plot([-0:0.1:200], repmat(rms3ML, 1, size(rms3, 2)), 'b--', 'LineWidth', 1.5);
 ylabel('RMS');
 xlabel('Alpha');
 legend('RMS_{MAP}', 'RMS_{ML}');
